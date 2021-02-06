2e4299a4c18be9de713df2dfc874029f28b7fb24
Lonami Exo <totufals@hotmail.com>
2021-02-06 23:13:26 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 set -e
 zola build
+git checkout gh-pages
 # Forgetting to exclude `.git` has costed me several days worth of work (and over 30 commits).
 find . -maxdepth 1 -not -name '.' -not -name '.git' -not -name 'public' -exec rm -r {} \;
 mv public/* .
+echo "lonami.dev" > CNAME
 rmdir public/
 git add .
 git commit --amend -m "Deploy site"
